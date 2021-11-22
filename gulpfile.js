8e75eb49630a14d44cde24a9625e9da74bac4a03
Lt. Morgan <ltmorgan2002@gmail.com>
2021-11-22 21:32:20 +0100

    promo section created

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ gulp.task('server', function() {
 
 // SASS Converter
 gulp.task('styles', function() {
-	return gulp.src("src/sass/*.+(scss|sass)")
+	return gulp.src("src/sass/**/*.+(scss|sass)")
 			.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 			.pipe(rename({					
 				prefix: "",
@@ -24,18 +24,15 @@ gulp.task('styles', function() {
 			}))
 			.pipe(autoprefixer())
 			.pipe(cleanCSS({compatibility: 'ie8'}))
-			.pipe(gulp.dest("src/css"))							
+			.pipe(gulp.dest("src/css"))
 			.pipe(browserSync.stream());
 });
 
 // File Change Tracking Task
 gulp.task('watch', function(){
-	gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"));
+	gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
 	gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
 // Gulp Default Task
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
-
-
-
